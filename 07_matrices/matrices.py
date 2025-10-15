44753a5bcac5bf7334f40b1ea2ed986a397c48d1
Erik Sánchez <ejsanchez@tec.mx>
2025-10-14 18:54:59 -0600

    Ejercicios de matrices

--- 07_matrices/matrices.py
+++ 07_matrices/matrices.py
@@ -6,7 +6,6 @@ def imprime_matriz(matriz):
 
 def imprime_matriz2(matriz):
     renglones = len(matriz)
-    columnas = len(matriz[0])
 
     for indice_renglon in range(renglones):
         renglon = matriz[indice_renglon]
@@ -14,16 +13,86 @@ def imprime_matriz2(matriz):
             print(matriz[indice_renglon][indice_columna], end = ", ")
         print("")
 
+def obtener_columna(matriz, indice):
+    renglones = len(matriz)
+    columna = []
 
-matriz = [
+    for indice_renglon in range(renglones):
+        renglon = matriz[indice_renglon]
+        for indice_columna in range(len(renglon)):
+            if indice_columna == indice:
+                columna.append(renglon[indice_columna])
+                break
+    return columna
+
+def crea_matriz(num_renglones, num_columnas, valor):
+    matriz = []
+
+    for n_ren in range(num_renglones):
+        renglon = []
+        for n_col in range(num_columnas):
+            renglon.append(valor)
+        matriz.append(renglon)
+    
+    return matriz
+
+def crea_matriz2(num_renglones, num_columnas, valor):
+    matriz = []
+
+    for n_ren in range(num_renglones):
+        renglon = [valor] * num_columnas
+        matriz.append(renglon)
+    
+    return matriz
+
+# matriz = crea_matriz(2, 3, "hola")
+# print(matriz)
+# print(crea_matriz2(2, 2, "hello"))
+
+# [
+#     ["hola", "hola", "hola"],
+#     ["hola", "hola", "hola"],
+# ]
+
+def suma_matrices(matriz_a, matriz_b):
+    if (len(matriz_a) == len(matriz_b)) and (len(matriz_a[0]) == len(matriz_b[0])):
+        resultado = []
+
+        for n_ren in range(len(matriz_a)):
+            renglon = []
+            
+            for n_col in range(len(matriz_a[n_ren])):
+                suma = matriz_a[n_ren][n_col] + matriz_b[n_ren][n_col]
+                renglon.append(suma)
+            
+            resultado.append(renglon)
+
+        return resultado
+    
+    print("No se pueden sumar las matrices")
+            
+def transponer_matriz(matriz):
+    return []
+
+def clonar_matriz(matriz):
+    return []
+
+digitos = [
+    #0  1  2
     [1, 2, 3], #0
-    [4, 5, 6, 19], #1
+    [4, 5, 6], #1
     [7, 8, 9], #2
 ]
 
-imprime_matriz(matriz)
-print("********************")
-imprime_matriz2(matriz)
+imprime_matriz(suma_matrices(digitos, digitos))
+
+
+
+# columna = obtener_columna(digitos, 1) # [2, 5, 8]
+# print(columna)
+# imprime_matriz(matriz)
+# print("********************")
+# imprime_matriz2(matriz)
 
 # matriz[0] # [1, 2, 3]
 # matriz[1] # [3, 4, 5]
